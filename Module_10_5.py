e50904b2f19f64e9e2dc4a65a1843b9bf62e6471
Misha31536 <e4ik@ya.ru>
2024-09-14 18:43:29 +0700

    add map method
    отредактировал линейный метод через map

--- Module_10_5.py
+++ Module_10_5.py
@@ -2,9 +2,9 @@ from time import perf_counter
 import multiprocessing
 
 def read_info(name):
-    all_data =[]
     with open(name, 'r') as file:
         all_data = [all_data.rstrip() for all_data in file]
+        return all_data
 
 filenames = [f'./file {number}.txt' for number in range(1, 5)]
 # 1 способ линейный через for
@@ -22,6 +22,6 @@ print(f'{finish - start:0.4f} сек линейный')
 #     print(f'{finish - start} сек линейный')
 #3й способ через map
 start = perf_counter()
-data = map(read_info, filenames)
+data = list(map(read_info, filenames))
 finish = perf_counter()
-print(f'{finish - start:0.40f} сек линейный через map')
+print(f'{finish - start:0.4f} сек линейный через map')
